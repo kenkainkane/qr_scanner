84bf7c8fccb456a6e1cbb213b6b42d45d9556d2d
kenkainkane <ken.auth99@gmail.com>
2021-09-05 17:33:08 +0700

    x

--- qr_scan_img.py
+++ qr_scan_img.py
@@ -2,12 +2,17 @@ import cv2
 import numpy as np
 import pyzbar.pyzbar as pyzbar
 
-image = cv2.imread("frame.png")
+image = cv2.imread("qr_test.png")
 
 decodedObjects = pyzbar.decode(image)
 for obj in decodedObjects:
-    print("Type:", obj.type)
-    print("Data: ", obj.data, "\n")
+    (x, y, w, h) = obj.rect
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    data = obj.data.decode("utf-8")
+    codeType = obj.type
+
+    cv2.putText(image, data, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
 cv2.imshow("Frame", image)
 cv2.waitKey(0)
+cv2.destroyAllWindows()
--- qr_scan_vdo.py
+++ qr_scan_vdo.py
@@ -11,7 +11,7 @@ while True:
     decodedObjects = pyzbar.decode(frame)
     for obj in decodedObjects:
         #print("Data", obj.data)
-        cv2.putText(frame, str(obj.data), (50, 50), font, 2,
+        cv2.putText(frame, str(obj.data.decode("utf-8")), (50, 50), font, 2,
                     (255, 0, 0), 3)
 
     cv2.imshow("Frame", frame)
